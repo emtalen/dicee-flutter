1867e083b3e9f291afb08d23e5d250304864bcb0
emtalen <59577366+emtalen@users.noreply.github.com>
2020-06-30 10:38:26 +0200

    refactoring the functionality of the onpress into  separate function

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class _DicePageState extends State<DicePage> {
 
   int rightDiceNumber = 2;
 
+  void changeDiceNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,9 +41,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+                changeDiceNumber();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -44,9 +49,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                changeDiceNumber();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
